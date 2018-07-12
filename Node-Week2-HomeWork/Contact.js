5d6de339f33c5cc476be7db8766616b473402bad
AmritaMaloo <amrita.maloo@gmail.com>
2018-07-12 12:56:48 +0200

    Adding index.js and Contact.js

---
+++ Node-Week2-HomeWork/Contact.js
@@ -0,0 +1,9 @@
+class Contact {
+    constructor(name, phone, bday) {
+        this.name = name;
+        this.phone = phone;
+        this.bday = bday;
+    }
+}
+
+module.exports = Contact;
---
+++ Node-Week2-HomeWork/index.js
@@ -0,0 +1,4 @@
+const Contact = require("./Contact.js");
+
+const instOfContact = new Contact("Amrita", 78798796, "Aug-7");
+console.log(instOfContact);
