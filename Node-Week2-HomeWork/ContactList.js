164b11520ad07af0d413721804a5ebfa3cfd945c
AmritaMaloo <amrita.maloo@gmail.com>
2018-07-12 13:41:14 +0200

    added save() method in ContactList.js and saved the contacts in file contacts.json

--- Node-Week2-HomeWork/ContactList.js
+++ Node-Week2-HomeWork/ContactList.js
@@ -1,8 +1,12 @@
 const Contact = require("./Contact");
+const fs = require("fs");
+const util = require("util");
+const writeFileP = util.promisify(fs.writeFile);
 
 class ContactList {
-    constructor() {
+    constructor(filename) {
         this.list = [];
+        this.filename = filename;
     }
 
     add (contact) {
@@ -11,6 +15,12 @@ class ContactList {
         else 
             console.log("Invalid Contact");
         
+    }
+
+    save () {
+        writeFileP(this.filename, JSON.stringify(this.list))
+        .then(() => console.log("File is saved"))
+        .catch(err => console.log(err));
     }
 }
 
--- Node-Week2-HomeWork/index.js
+++ Node-Week2-HomeWork/index.js
@@ -1,8 +1,10 @@
 const Contact = require("./Contact.js");
 const ContactList = require("./ContactList.js");
 
-const instOfContact = new Contact("Amrita", 78798796, "Aug-7");
-const instOfContactList = new ContactList();
+const instOfContact = new Contact("A", 78798796, "Aug-7");
+const instOfContactList = new ContactList("contacts.json");
 instOfContactList.add(instOfContact);
+instOfContactList.add(new Contact("B", null, "Sep-7"));
+instOfContactList.save();
 console.log(instOfContact);
 console.log(instOfContactList);
