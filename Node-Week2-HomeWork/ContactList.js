68035041a1c9126297ed1b244e1fcbf1d77a125b
AmritaMaloo <amrita.maloo@gmail.com>
2018-07-13 06:40:53 +0200

    load method added

--- Node-Week2-HomeWork/ContactList.js
+++ Node-Week2-HomeWork/ContactList.js
@@ -2,6 +2,7 @@ const Contact = require("./Contact");
 const fs = require("fs");
 const util = require("util");
 const writeFileP = util.promisify(fs.writeFile);
+const readFileP = util.promisify(fs.readFile);
 
 class ContactList {
     constructor(filename) {
@@ -20,6 +21,21 @@ class ContactList {
     save () {
         writeFileP(this.filename, JSON.stringify(this.list))
         .then(() => console.log("File is saved"))
+        .catch(err => console.log(err));
+    }
+
+    load () {
+        readFileP(this.filename)
+        .then((data) => { 
+            
+            let parsed = JSON.parse(data);
+            for(const element of parsed) {
+            let parsedInstOfContact = new Contact(element.name, element.phone, element.bday);
+            console.log(parsedInstOfContact);
+            }
+                       
+        })
+        
         .catch(err => console.log(err));
     }
 }
--- Node-Week2-HomeWork/index.js
+++ Node-Week2-HomeWork/index.js
@@ -5,6 +5,12 @@ const instOfContact = new Contact("A", 78798796, "Aug-7");
 const instOfContactList = new ContactList("contacts.json");
 instOfContactList.add(instOfContact);
 instOfContactList.add(new Contact("B", null, "Sep-7"));
+instOfContactList.add(new Contact("C", 87573409, "Oct-7"));
+instOfContactList.add(new Contact("D", 78543987, null));
+instOfContactList.add(new Contact("E", 67542098, "Nov-7"));
+instOfContactList.add(new Contact("F", 87590956, "Dec-7"));
+
 instOfContactList.save();
-console.log(instOfContact);
+instOfContactList.load();
+//console.log(instOfContact);
 console.log(instOfContactList);
